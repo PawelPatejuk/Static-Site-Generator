da5d242d225e4a1c973eecf1bef0f31aa405206f
PawelPatejuk <patatajuk@gmail.com>
2025-04-25 22:44:34 +0200

    third commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 from textnode import TextNode
 from markdown import markdown_to_html_node
@@ -8,7 +9,7 @@ from htmlnode import HTMLNode
 
 def main():
 	copy_files("static", "public")
-	generate_page("content/index.md", "template.html", "public/index.html")
+	generate_pages_recursive("content", "template.html", "public")
 
 
 def copy_files(src_dir, dest_dir):
@@ -28,10 +29,14 @@ def copy_files(src_dir, dest_dir):
 
 
 def extract_title(markdown):
+#	if len(markdown.strip()) == 0:
+#		return "heeey"
+
 	for line in markdown.split("\n"):
 		if line.startswith("# "):
 			return line[1:].strip()
 
+	print(f"\n\nmarkdown:\n{markdown}")
 	raise Exception("heading not found")
 
 
@@ -57,6 +62,29 @@ def generate_page(from_path, template_path, dest_path):
 	f.write(new_content)
 	f.close()
 
+	print("generate_page: Done\n")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+	if not os.path.exists(dest_dir_path) and not os.path.isfile(dest_dir_path):
+		os.mkdir(dest_dir_path)
+
+	for item in os.listdir(dir_path_content):
+		src_item = os.path.join(dir_path_content, item)
+		dest_item = os.path.join(dest_dir_path, item)
+
+		print(f"\nsrc_item: {src_item}")
+
+		if os.path.isfile(src_item) and src_item.endswith(".md"):
+			print(f"generated page: {src_item}")
+			dest_item = dest_item.replace(".md", ".html")
+			generate_page(src_item, template_path, dest_item)
+
+		elif os.path.isdir(src_item):
+			print(f"recursive page: {src_item}")
+			generate_pages_recursive(src_item, template_path, dest_item)
+
+	print("generate_pages_recursive: Done\n")
 
 
 main()
+
